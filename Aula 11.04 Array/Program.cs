0b4cbd021b5cdeee1866cd081fa972eb02b2a50a
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-04-11 11:06:06 -0300

    11.04.23

---
+++ Aula 11.04 Array/Program.cs
@@ -0,0 +1,79 @@
+﻿//Aula 11/04
+//criar um array de strings para armaenar 3 carros
+//string[] carros = new string[3];
+//for (var i = 0; i < 3; i++)
+//{
+//    Console.WriteLine($"Informe o nome do carro:");
+//    carros[i] = Console.ReadLine();
+//}
+//Console.WriteLine($"carros cadastrados com sucesso!");
+//Console.WriteLine($"Primeiro carro: {carros[0]}");
+//Console.WriteLine($"Segundo carro: {carros[1]}");
+//Console.WriteLine($"Terceiro carro: {carros[2]}");
+
+//Console.WriteLine($"lista dos carros cadastrados:");
+//for(var i = 0; i < 3; i++)
+//{
+//  Console.WriteLine($"Nome do carro: {carros[i]}");
+//}
+// foreach (var item in collection)
+// Console.WriteLine(item);
+
+
+
+
+//Atividade aula 11.04
+
+//criar um programa em C# que receba 5 numeros inteiros e ao final exiba o seu dobro
+//voce deve utilizar array com tamanho 5 para o armazenamento dos numeros,
+// a estrutura for para a leitura dos numeros e a estrutura foreach para exibi-los
+//entrada
+//criar o array tamanho 5
+//int[] numero = new int[5];
+//[5] = A /0 B/1 C/2 D/3 E/4
+// sao 5 gavetas mas iniciando com zero]
+//processamento
+//receber os numeros com (for)
+//for (int indice = 0; indice < 5; indice++)
+//{
+//    Console.WriteLine($"Digite o {indice + 1}º numero");
+//    numero [indice] = int.Parse(Console.ReadLine());
+//}
+//saida
+//exibir os numeros com (foreach)
+//foreach(var item in numero){
+// Exemplo se uma pessoa digitou
+//5
+//10
+//15
+//30
+//100
+//Console.WriteLine($"Digite o dobro de {item} e {item*2}");
+//}
+
+
+
+
+//Exercicio de fixacao
+//Escreva um programa que receba e imprima o nome e a idade de 5 pessoas
+//personalizar cores: a resposta do nome em azul e a resposta da idade em verde
+
+//Exemplo de exibicao
+//1) nome: Carlos
+//idade: 36 anos
+//2) nome: Eduardo
+//idade: 40 anos
+//3) nome: Allan
+//idade: 30 anos
+//4) nome: Julia
+//idade: 18 anos
+//5) nome: Gustavo
+//idade: 16 anos
+
+// Criar lista os 5nomes e uma lista de idades
+//mandar o computador alinhar nome na posicao com a posicao da idade
+
+
+
+
+
